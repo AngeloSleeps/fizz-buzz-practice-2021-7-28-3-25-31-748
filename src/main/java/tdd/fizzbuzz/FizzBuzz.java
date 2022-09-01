749f5a2715853438e370688434820187d032180e
Marcelo Magday <marcelo.angelo.magday@oocl.com>
2022-09-01 14:26:39 +0800

    refactor: put Whizz as final String in FizzBuzz Class

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,7 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
     private static final String FIZZ = "Fizz";
     private static final String BUZZ = "Buzz";
+    private static final String WHIZZ = "Whizz";
     public String countOf(int order) {
         StringBuilder word = new StringBuilder();
         if(order % 3 == 0 && order % 5 == 0) {
@@ -11,7 +12,7 @@ public class FizzBuzz {
         } else if (order % 5 == 0) {
             word.append(BUZZ);
         } else if (order % 7 == 0) {
-            word.append("Whizz");
+            word.append(WHIZZ);
         } else if (order % 3 == 0) {
             word.append(FIZZ);
         }
