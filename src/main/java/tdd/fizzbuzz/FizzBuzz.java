23ab4430675e7ff75bdea068dac29084bade41ac
Marcelo Magday <marcelo.angelo.magday@oocl.com>
2022-09-01 14:42:44 +0800

    feat: return FizzBuzzWhizz when multiple of 3 and 5 and 7

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -4,9 +4,15 @@ public class FizzBuzz {
     private static final String FIZZ = "Fizz";
     private static final String BUZZ = "Buzz";
     private static final String WHIZZ = "Whizz";
+
     public String countOf(int order) {
         StringBuilder word = new StringBuilder();
-        if(order % 3 == 0 && order % 5 == 0) {
+        if (order % 3 == 0 && order % 5 == 0 && order % 7 == 0) {
+            word.append(FIZZ)
+                    .append(BUZZ)
+                    .append(WHIZZ);
+        }
+        else if (order % 3 == 0 && order % 5 == 0) {
             word.append(FIZZ)
                     .append(BUZZ);
         } else if (order % 3 == 0 && order % 7 == 0) {
