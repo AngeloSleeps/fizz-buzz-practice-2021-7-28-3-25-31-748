321622a0a55193696a66586e663b4229316d587c
Marcelo Magday <marcelo.angelo.magday@oocl.com>
2022-09-01 14:17:00 +0800

    refactor: put buzz string as static constant in FizzBuzz class

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,12 +2,13 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
     public String countOf(int order) {
         StringBuilder word = new StringBuilder();
         if(order % 3 == 0) {
             word.append(FIZZ);
         } else if (order % 5 == 0) {
-            word.append("Buzz");
+            word.append(BUZZ);
         }
         return word.toString();
     }
