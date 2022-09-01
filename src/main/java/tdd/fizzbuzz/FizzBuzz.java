a8c5d4963e29b97b00549b9e2bd2a68911812c23
Marcelo Magday <marcelo.angelo.magday@oocl.com>
2022-09-01 14:25:33 +0800

    feat: return FizzBuzz when multiple of 3 and 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -5,12 +5,15 @@ public class FizzBuzz {
     private static final String BUZZ = "Buzz";
     public String countOf(int order) {
         StringBuilder word = new StringBuilder();
-        if(order % 3 == 0) {
-            word.append(FIZZ);
+        if(order % 3 == 0 && order % 5 == 0) {
+            word.append(FIZZ)
+                    .append(BUZZ);
         } else if (order % 5 == 0) {
             word.append(BUZZ);
         } else if (order % 7 == 0) {
             word.append("Whizz");
+        } else if (order % 3 == 0) {
+            word.append(FIZZ);
         }
         return word.toString();
     }
